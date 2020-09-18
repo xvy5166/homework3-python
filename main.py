9a6bff93eae8648590fb225d3e04020da5ebab2e
XinyiY5166 <xvy5166@psu.edu>
2020-09-18 03:47:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def n_sum(n):
+  if n == 0:
+    return 0
+  else:
+    return n%10+n_sum(n//10)
+
+
+def run():
+  i = int(input("Enter an int: "))
+  num_sum = n_sum(i)
+  print(f"sum of digits of {i} is {num_sum}.")
+
+
+if __name__ == "__main__":
+  run()
+
+
+
+

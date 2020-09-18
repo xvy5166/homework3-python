ced11a9316a41e5cadbffeaa60ac78bc3e82881d
XinyiY5166 <xvy5166@psu.edu>
2020-09-18 03:52:16 +0000

    function name

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #Author: Xinyi Yang xvy5166@psu.edu
 
 
-def n_sum(n):
+def digit_sum(n):
   if n == 0:
     return 0
   else:
-    return n%10+n_sum(n//10)
+    return n%10+digit_sum(n//10)
 
 
 def run():
   i = int(input("Enter an int: "))
-  num_sum = n_sum(i)
+  num_sum = digit_sum(i)
   print(f"sum of digits of {i} is {num_sum}.")
 
 
